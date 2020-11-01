0378b2912f65a4b3c89ea0f40d11169622cc692b
msongfrancis <leex6165@umn.edu>
2020-11-01 21:53:18 +0000

    filter for MN data and fields of interest

--- SafeGraph.py
+++ SafeGraph.py
@@ -2,12 +2,22 @@ import gzip
 import glob
 import csv
 import geopandas as gpd
-
+import pandas as pd
 
 inFile = 'jan19patterns-part1.csv.gz'
 
-# open gz file and obtain field names
+# Obtain fieldnames from file
+inFile = 'jan19patterns-part1.csv.gz'
+
 with gzip.open(inFile, 'rt') as jan19:
-    reader = csv.DictReader(jan19)
-    fields = []
-    fields.append(reader.fieldnames)
+    reader = pd.read_csv(jan19)
+    
+    # Extract list of column names and create new data frame with fields of interest
+    all_col = []
+    for col in reader.head():
+        all_col.append(col)
+    ex_col = all_col[:11] # fields of interest are first 12 fields
+    df = reader.filter(ex_col)
+ 
+# Filter for data in MN
+mn_df = df.loc[df['region'] == 'MN']
